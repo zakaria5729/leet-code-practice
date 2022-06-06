afe5ff47a1ae3e0434358475014972eae0f3885e
zakaria5729 <zakariahossain143@gmail.com>
2022-06-06 11:08:28 +0600

    solve shuffle string problem

---
+++ easy/ShuffleString1528.java
@@ -0,0 +1,13 @@
+// problem link: https://leetcode.com/problems/shuffle-string/
+
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        StringBuilder sb = new StringBuilder(s);
+        
+        for(int i = 0; i < indices.length; i++) {
+            sb.setCharAt(indices[i], s.charAt(i));
+        }
+        
+        return sb.toString();
+    }
+}
