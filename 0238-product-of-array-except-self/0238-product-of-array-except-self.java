c79748545d14d75a50dc70e474de318863783071
Zakaria Hossain <zakariahossain143@gmail.com>
2023-11-13 21:35:55 +0600

    Time: 2 ms (65.14%), Space: 51.6 MB (78.21%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,49 +1,20 @@
-class Solution {
+public class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length, mult = 1;
-    int[] ans = new int[n];
-        
-    for(int i = 0; i<n; i++){
-        ans[i] = mult;
-        mult*=nums[i];
-        
-        System.out.println("i: " + i + "ans[i]: " + ans[i] + "mult: " + mult);
-    }
-        
-    System.out.println();
-    
-    mult = 1;
-    for(int j = n-1; j>=0; j--){
-        ans[j]*=mult;
-        mult*=nums[j];
-        
-        System.out.println("j: " + j + "ans[j]: " + ans[j] + "second mult: " + mult);
-    }
-        
-    return ans;
-        
-//         int[] results = new int[nums.length];
-        
-//         for (int i = 0; i < nums.length; i++) {
-//             results[i] = getSum(nums, i);
-//         }
-        
-//         return results;
-    }
-    
-    private int getSum(int[] nums, int j) {
-        int sum = 1;
+        int temp = 1;
+        int[] results = new int[nums.length];
         
         for (int i = 0; i < nums.length; i++) {
-            if (i != j) {
-                sum *= nums[i];    
-            }
-            
-            if (sum == 0) {
-                break;
-            }
+            results[i] = temp;
+            temp *= nums[i];
         }
         
-        return sum;
+        temp = 1;
+        
+        for (int j = nums.length -1; j >= 0; j--) {
+            results[j] *= temp;
+            temp *= nums[j];
+        }
+        
+        return results;
     }
 }
