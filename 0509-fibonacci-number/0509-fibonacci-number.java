c9e31779271aca2efc701355bb89daa9002fee1c
Zakaria Hossain <zakariahossain143@gmail.com>
2023-11-16 16:51:23 +0600

    Time: 0 ms (100.00%), Space: 39.3 MB (30.30%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,18 @@
+public class Solution {
+    final Map<Integer, Integer> memo = new HashMap<>();
+    
+    public int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        
+        Integer value = memo.get(n);
+        
+        if (value == null) {
+            value = fib(n - 1) + fib(n - 2);
+            memo.put(n, value);
+        }
+        
+        return value;
+    }
+}
