b21d555f2785d73976140e4473bc20f4387a05e7
Zakaria Hossain <zakariahossain143@gmail.com>
2023-11-22 18:18:35 +0600

    Time: 21 ms (30.55%), Space: 44.2 MB (20.38%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -4,7 +4,7 @@ public class Solution {
         if (s.isEmpty()) return true;
         s = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
         
-        for (int i = 0, j = s.length() - 1; i < s.length(); i++, j--) {
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
             if (s.charAt(i) != s.charAt(j)) return false;
         }
         
