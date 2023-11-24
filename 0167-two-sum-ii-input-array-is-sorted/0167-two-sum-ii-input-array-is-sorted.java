3e3fa600a47a35afdb012738b90e901b66d697f8
Zakaria Hossain <zakariahossain143@gmail.com>
2023-11-24 16:40:53 +0600

    Update 0167-two-sum-ii-input-array-is-sorted.java

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -3,20 +3,12 @@ public class Solution {
         
         int left = 0;
         int right = numbers.length - 1;
-        int[] results = new int[2];
         
-        while (left < right) {
-            int sum = numbers[left] + numbers[right];
-            
-            if (sum == target) {
-                results[0] = left + 1;
-                results[1] = right + 1;
-                break;
-            }
-            
-            if (sum < target) left++; else right--;
+        while (numbers[left] + numbers[right] != target) {
+            if (numbers[left] + numbers[right] < target) left++;
+            else right--;
         }
         
-        return results;
+        return new int[] {left + 1, right + 1};
     }
 }
